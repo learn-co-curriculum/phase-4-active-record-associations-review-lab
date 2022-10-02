b84a60eabbe993a7a1b9601cbcabd5921d1bbb70
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-02 22:49:09 +0300

    commit tree 126e4547d19ac1740225727aa12716610e3dcf61

--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,4 @@
 class Taxi < ApplicationRecord
+    has_many :rides
+    has_many :passengers, through: :rides
 end
--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
+      t.belongs_to :taxi
+      t.belongs_to :passenger
 
       t.timestamps
     end
